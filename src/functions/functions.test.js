8a05540df62ee192ace0630348db6fff1678be09
manisha1896 <manisha.4918@gmail.com>
2019-03-07 17:48:25 +0530

    feat(function): method that run after/before each test case

--- src/functions/functions.test.js
+++ src/functions/functions.test.js
@@ -1,5 +1,12 @@
 const functions = require("./functions");
 
+const start = () => console.log("Test start...");
+const end = () => console.log("Test End !!");
+
+// Jest will wait for these promise to resolve before and after running each test.
+beforeEach(() => start());
+afterEach(() => end());
+
 describe("basic arithmetic function", () => {
   test("adds 463 + 148 to equal to 611", () => {
     expect(functions.add(463, 148)).toBe(611);
