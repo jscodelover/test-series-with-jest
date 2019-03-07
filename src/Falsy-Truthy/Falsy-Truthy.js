48294cc103df8e2d9b7576e0f0ef518fc220545a
manisha1896 <manisha.4918@gmail.com>
2019-03-07 13:34:10 +0530

    feat(Falsy-Truthy): test case for null (toBeNull)

---
+++ src/Falsy-Truthy/Falsy-Truthy.js
@@ -0,0 +1,9 @@
+const Falsy_Truthy = {
+  isNull() {
+    return null;
+  }
+};
+
+module.exports = {
+  isNull: Falsy_Truthy.isNull
+};
---
+++ src/Falsy-Truthy/Falsy-Truthy.test.js
@@ -0,0 +1,16 @@
+/**
+ * CHECK FOR TRUTHY & FALSY VALUES
+ * toBeNull matches only null
+ * toBeUndefined matches only undefined
+ * toBeDefined is the opposite of toBeUndefined
+ * toBeTruthy matches anything that an if statement treats as true
+ * toBeFalsy matches anything that an if statement treats as false
+ */
+
+const Falsy_Truthy = require("./Falsy-Truthy");
+
+describe("CHECK FOR TRUTHY & FALSY VALUES", () => {
+  test("Value is null", () => {
+    expect(Falsy_Truthy.isNull()).toBeNull();
+  });
+});
