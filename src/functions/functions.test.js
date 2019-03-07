3b445f4438413beeacc0f70c658ffbe21efbeaaf
manisha1896 <manisha.4918@gmail.com>
2019-03-07 12:31:22 +0530

    refactor(function): group the test cases

--- src/functions/functions.test.js
+++ src/functions/functions.test.js
@@ -1,5 +1,7 @@
 const functions = require("./functions");
 
-test("adds 463 + 148 to equal to 611", () => {
-  expect(functions.add(463, 148)).toBe(611);
+describe("basic arithmetic function", () => {
+  test("adds 463 + 148 to equal to 611", () => {
+    expect(functions.add(463, 148)).toBe(611);
+  });
 });
