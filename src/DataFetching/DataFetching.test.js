93c743becbb441e8d6fbbf3a9267a201c29603cb
manisha1896 <manisha.4918@gmail.com>
2019-03-07 16:41:31 +0530

    feat(DataFetch): test case for user fetching using asyn/await

--- src/DataFetching/DataFetching.test.js
+++ src/DataFetching/DataFetching.test.js
@@ -1,10 +1,17 @@
 const Fetch = require("./DataFetching");
 
 describe(" Test cases for fetched data", () => {
+  // Using Promise
   test("fetched user name is Bret", () => {
     expect.assertions(1);
     return Fetch.getUserName().then(data => {
       expect(data.username).toEqual("Bret");
     });
+  });
+  // Using Async-Await
+  test("fetched user name is Bret", async () => {
+    expect.assertions(1);
+    const data = await Fetch.getUserName();
+    return expect(data.username).toEqual("Bret");
   });
 });
