f85ce0c49b5956c49cc0e60bea36784ae21fac50
manisha1896 <manisha.4918@gmail.com>
2019-03-07 17:42:56 +0530

    feat(datafetch): test case that run before/after all test cases

--- src/DataFetching/DataFetching.test.js
+++ src/DataFetching/DataFetching.test.js
@@ -1,5 +1,12 @@
 const Fetch = require("./DataFetching");
 
+const initDatabase = () => console.log("Database Initialising....");
+const closeDatabase = () => console.log("Database Closing !!!");
+
+// Jest will wait for these promise to resolve before and after running tests.
+beforeAll(() => initDatabase());
+afterAll(() => closeDatabase());
+
 describe(" Test cases for fetched data", () => {
   // Using Promise
   test("fetched user name is Bret", () => {
